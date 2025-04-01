4482b181be900dcb85d6c290367bfef93eb31749
dewang002 <2002thapa@gmail.com>
2025-04-01 17:22:49 +0530

    doen

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const say = require('say')
 const cors = require('cors')
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://fetexttovoice.vercel.app/'],
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.post("/talk",(req,res)=> {
